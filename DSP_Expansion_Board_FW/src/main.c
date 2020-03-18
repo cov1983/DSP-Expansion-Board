85f26265b533c37a06ccd9598c1f2f471f89de82
iceMan83 <thomas.cova@helbling.ch>
2020-03-18 22:32:14 +0100

    Cleaned Up Main, just minimal initialization left and statemachine call.

--- DSP_Expansion_Board_FW/src/main.c
+++ DSP_Expansion_Board_FW/src/main.c
@@ -19,18 +19,16 @@
 //! Short description of this file/module.
 //==================================================================================================
 
-
 //==================================================================================================
 //  I N C L U D E D   F I L E S
 //==================================================================================================
 
-#include "em_device.h" 	// Includes the MCU dependent headers based on the project's MCU definition
-#include "em_lcd.h"		// emblib for LCD
+#include "em_lcd.h"			// emblib for LCD
 
-#include "segmentlcd.h" // Include STK3800 LCD Library
+#include "segmentlcd.h" 	// Include STK3800 LCD Library
 
-#include "system.h"		// Include System Module
-#include "codec.h"		// Include CODEC Module
+#include "system.h"			// Include System Module
+#include "statemachine.h"	// Include Statemachine Module
 
 //==================================================================================================
 //  M O D U L E   D E F I N E S
@@ -44,20 +42,13 @@
 //  M O D U L E   V A R I A B L E S
 //==================================================================================================
 
-bool dspBoardActive = false;
-bool i2cError = false;
-
-bool buttonPB0once = false;
-
 //==================================================================================================
 //  F O R W A R D   D E C L A R A T I O N S
 //==================================================================================================
 
-
 //==================================================================================================
 //  G L O B A L   F U N C T I O N   I M P L E M E N T A T I O N
 //==================================================================================================
-
 
 //==================================================================================================
 //  M O D U L E   F U N C T I O N   I M P L E M E N T A T I O N
@@ -80,25 +71,21 @@ bool buttonPB0once = false;
 
 int main(void)
 {
-	// STK3800 BASIC SYSTEM INIT
-	initSTK3800_Basic();
-
+	// Initialize STK3800
+	initSTK3800();
 
 	// Initialize LCD without boost
 	SegmentLCD_Init(false);
+
 	// Show welcome message on LCD
 	SegmentLCD_AllOff();
 	SegmentLCD_Write("DSPDemo");
-	Delay(2000);
-
-	// Initialize STK3800
-	initSTK3800_Basic();
-
+	Delay(3000);
 
 	// ENTER INFINITE LOOP
 	while (true)
 	{
-
+		// Check if DSP Expansion Board is Active
 		if (getDSPDetectionFlag())
 		{
 			SegmentLCD_Write("DSP ON");
@@ -107,62 +94,12 @@ int main(void)
 		}
 		else {
 			SegmentLCD_Write("DSP OFF");
+			// Disable Interface to DSP Expansion Board
+			// ...
 		}
-
-
-
-
-
-
-
-		// Check if PB0 Button is activated
-		/*
-		if (getButtonPB0Flag() && (!buttonPB0once))
-		{
-			// Enable LED0
-			enableLED0();
-
-			// CODEC SETUP LINE IN TO HP OUT AT FS = 48Khz
-			// CODEC Base Initialization (LDO Power)
-			i2cError = initCODEC();
-			// CODEC PLL & Clocks Initialization
-			i2cError = setSampling48CODEC();
-			// CODEC Configure Processing Blocks
-			i2cError = setProcessingBlocksCODEC();
-			// CODEC DAC Routing & Power Up
-			i2cError = setDacHPCODEC();
-			Delay(100);
-			// CODEC ADC Routing & Power Up
-			i2cError = setAdcIN2CODEC();
-			// CODEC Set Default Digital Gain ADC L&R 0dB
-			i2cError = setAdcDigitalGainCODEC();
-
-			// Set flag for ButtonPB0 pressed once
-			buttonPB0once = true;
-		}
-		else {
-			// Disable LED0
-			disableLED0();
-		}
-
-		// Check if PB1 Button is activated
-		if (getButtonPB1Flag())
-		{
-			// Enable LED1
-			enableLED1();
-		}
-		else {
-
-			// Disable LED1
-			disableLED1();
-		}
-		*/
 	}
 	return (0);
 }
-
-
-
 
 //==================================================================================================
 //  E N D   O F   F I L E
